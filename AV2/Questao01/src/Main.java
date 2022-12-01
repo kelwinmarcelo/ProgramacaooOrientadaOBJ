ea615abb7e5c4bb58bda9dc34b5d62ad9c431776
Patrick Dias Siqueira <12114788@aluno.newtonpaiva.br>
2022-12-01 20:39:14 -0300

    questão 02

--- AV2/Questao01/src/Main.java
+++ AV2/Questao01/src/Main.java
@@ -9,23 +9,21 @@ public class Main {
         String nomeUsuario = JOptionPane.showInputDialog("NOME: ");
         String dataNascimento = JOptionPane.showInputDialog("DATA DE NASCIMENTO: ");
         String senha = JOptionPane.showInputDialog("SENHA: ");
-
         RedeSocial rede1 = new RedeSocial(dataCriacao, nomeUsuario, dataNascimento, senha);
-
         String dataPublicacao = new Date().toString();
         String textoPublicacao = JOptionPane.showInputDialog("TEXTO A PUBLICAR: ");
         String linksMideia = JOptionPane.showInputDialog("LINK DA PUBLICACAO: ");
 
         Publicacao publi = new Publicacao(dataPublicacao, textoPublicacao, linksMideia);
-
         dataPublicacao = new Date().toString();
+        
         textoPublicacao = JOptionPane.showInputDialog("TEXTO A PUBLICAR: ");
         linksMideia = JOptionPane.showInputDialog("LINK DA PUBLICACAO: ");
-
         publi = new Publicacao(dataPublicacao, textoPublicacao, linksMideia);
 
-        rede1.inserePublicacao(publi);
 
+        
+        rede1.inserePublicacao(publi);
         rede1.imprimePublicacoes();
     }
 }
